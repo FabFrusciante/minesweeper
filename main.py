ea7dcd1f0bce644ed02c5d6ada7dea7b3d52a967
Fabian Barthold <79047433+FabFrusciante@users.noreply.github.com>
2021-02-14 15:22:42 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+from random import randint
+
+field_size = 8
+
+
+def create_mine_field():
+    mine_field = {}
+    for x in range(0, field_size):
+        for y in range(0, field_size):
+            mine_field.update({(x, y): randint(0, 1)})
+    return mine_field
+
+
+def create_visual():
+    visual = {}
+    for x in range(0, field_size):
+        for y in range(0, field_size):
+            visual.update({(x, y): "x"})
+    return visual
+
+
+def print_visual(visual):
+    for x in range(0, field_size):
+        for y in range(0, field_size):
+            print(visual[(x, y)], " ", end="")
+        print("")
+
+
+def is_mine(feld, x, y):
+    if feld[x, y] == 0:
+        return False
+    return True
+
+
+def update_visual(visual, x, y):
+    visual[(x, y)] = 0
+    visual = visual + 3
+
+
+def main():
+    mine_field = create_mine_field()
+    visual = create_visual()
+
+    while True:
+        print_visual(visual)
+        x = int(input("x Koordinate: "))
+        y = int(input("y Koordinate: "))
+
+        if is_mine(mine_field, x, y):
+            print("Mine. Spiel ist vorbei.")
+            break
+        print("Nichts")
+        update_visual(visual, x, y)
+
+
+if __name__ == '__main__':
+    main()
