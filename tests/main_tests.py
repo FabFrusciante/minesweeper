ff0d084b67e62d81cb6c5e3eb009432a91eab010
FabFrusciante <fabian-barthold@gmx.de>
2021-02-27 18:07:05 +0100

    add test for is_mine

--- tests/main_tests.py
+++ tests/main_tests.py
@@ -1,4 +1,4 @@
-from main import create_mine_field
+from main import create_mine_field, is_mine
 
 
 def test_create_mine_field():
@@ -10,3 +10,15 @@ def test_create_mine_field():
     for value in field.values():
         if value not in [0, 1]:
             assert False
+
+
+def test_is_mine():
+    field = {(1, 2): 0}
+    x = 1
+    y = 2
+    mine = is_mine(field, x, y)
+    assert isinstance(mine, bool)
+    assert mine is False
+    field = {(1, 2): 1}
+    mine = is_mine(field, x, y)
+    assert mine is True
