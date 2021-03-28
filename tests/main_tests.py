e7126a45b69d004c3f9fcb0ca293455f2ab6a59b
RMeissner <robert.meissner@codecentric.de>
2021-03-28 15:59:14 +0200

    - WIP: tests

--- tests/main_tests.py
+++ tests/main_tests.py
@@ -3,13 +3,7 @@ from main import create_mine_field, is_mine
 
 def test_create_mine_field():
     field = create_mine_field()
-    assert field is not None
-    assert isinstance(field, dict)
-    print(len(field))
-    assert len(field) == 64
-    for value in field.values():
-        if value not in [0, 1]:
-            assert False
+    assert isinstance(field, list)
 
 
 def test_is_mine():
@@ -19,6 +13,7 @@ def test_is_mine():
     mine = is_mine(field, x, y)
     assert isinstance(mine, bool)
     assert mine is False
+
     field = {(1, 2): 1}
     mine = is_mine(field, x, y)
     assert mine is True
