f549a221e78e35e9d25d6773fbc3431444ee27c5
RMeissner <robert.meissner@codecentric.de>
2021-03-28 17:50:51 +0200

    - Updating visual field to numpy array

--- main.py
+++ main.py
@@ -10,17 +10,14 @@ def create_mine_field():
 
 
 def create_visual():
-    visual = {}
-    for x in range(0, field_size):
-        for y in range(0, field_size):
-            visual.update({(x, y): "x"})
-    return visual
+    visual = np.array(["x" for _ in range(0, field_size ** 2)])
+    return visual.reshape(field_size, field_size)
 
 
-def print_visual(visual):
+def print_visual(visual: np.ndarray):
     for x in range(0, field_size):
         for y in range(0, field_size):
-            print(visual[(x, y)], " ", end="")
+            print(visual[x, y], " ", end="")
         print("")
 
 
@@ -30,9 +27,8 @@ def is_mine(field: np.ndarray, x: int, y: int) -> bool:
     return True
 
 
-def update_visual(visual, x, y):
-    visual[(x, y)] = 0
-    visual = visual + 3
+def update_visual(visual: np.ndarray, x: int, y: int) -> None:
+    visual[x, y] = 0
 
 
 def main():
